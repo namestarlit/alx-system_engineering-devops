2831c7ba875a977781e79fc58bff476b5766aa0a
Paul John <rainson.work@gmail.com>
2023-06-25 18:24:49 +0300

    Modify the manifest

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,12 @@
-# set sh_config file.
-file_line { 'Declare_identity_file':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'IdentityFile ~/.ssh/school',
-  owner => 'user',
-  group => 'user',
-  mode  => '0600',
+# Puppet script to create ssh config file
+file_line { 'Set_PasswordAuthentication':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
 
-file_line { 'Turn_off_passwd_auth':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'PasswordAuthentication no',
-  owner => 'user',
-  group => 'user',
-  mode  => '0600',
+file_line { 'Declare_IdentityFile':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
