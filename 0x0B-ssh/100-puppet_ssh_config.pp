5e06fbcd9cb89f92e921574f627f4a3e54706cf5
Paul John <rainson.work@gmail.com>
2023-06-25 18:04:20 +0300

    Add `100-puppet_ssh_config.pp`

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+file {'/home/ubuntu/.ssh/config':
+  ensure    => present,
+  mode      => '0600',
+  owner     => 'ubuntu',
+  group     => 'ubuntu',
+  content   => "
+  Host 52.87.155.27
+	  IdentityFile ~/.ssh/school
+	  PasswordAuthentication no
+  ",
+  show_diff => true,
+}
