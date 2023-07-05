7be4a44cbb26bfc3ed02de6ebaff5fdacc5147cb
Paul John <rainson.work@gmail.com>
2023-07-05 20:35:39 +0300

    Modify manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,9 +3,30 @@
 package { 'nginx':
   ensure => installed,
 }
+file { '/etc/nginx/sites-available':
+  ensure  => directory,
+  recurse => true,
+  purge   => true,
+  force   => true,
+}
 
-file {'/var/www/html/index.nginx-debian.html':
-  ensure => absent,
+file { '/etc/nginx/sites-enabled':
+  ensure => directory,
+}
+
+file { '/var/www/html':
+  ensure => directory,
+}
+
+file { '/var/www/bak':
+  ensure => directory,
+}
+
+exec { 'backup_index_file':
+  command     => 'mv /var/www/html/index.nginx-debian.html /var/www/bak/',
+  onlyif      => '[ -f /var/www/html/index.nginx-debian.html ]',
+  path        => '/usr/bin:/usr/sbin:/bin',
+  refreshonly => true,
 }
 
 file {'/var/www/html/index.html':
