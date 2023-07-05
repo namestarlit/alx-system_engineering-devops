6371ba6d441f66da47cdd1a7ac8cef04afe5bbd4
Paul John <rainson.work@gmail.com>
2023-07-05 19:36:24 +0300

    Modify manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,64 +4,48 @@ package { 'nginx':
   ensure => installed,
 }
 
-file { '/etc/nginx/sites-available':
-  ensure  => directory,
-  recurse => true,
-  purge   => true,
-  force   => true,
+file {'/var/www/html/index.nginx-debian.html':
+  ensure => absent,
 }
 
-file { '/etc/nginx/sites-enabled':
-  ensure => directory,
-}
-
-file { '/var/www/html':
-  ensure => directory,
-}
-
-file { '/var/www/bak':
-  ensure => directory,
-}
-
-exec { 'backup_index_file':
-  command     => 'mv /var/www/html/index.nginx-debian.html /var/www/bak/',
-  onlyif      => '[ -f /var/www/html/index.nginx-debian.html ]',
-  path        => '/usr/bin:/usr/sbin:/bin',
-  refreshonly => true,
-}
-
-file { '/var/www/html/index.html':
+file {'/var/www/html/index.html':
   ensure  => present,
   content => 'Hello World!',
 }
 
-file { '/etc/nginx/sites-available/default':
-  content => @("EOF"),
-server {
-  listen 80 default_server;
-  listen [::]:80 default_server;
-  add_header X-Served-By \$hostname;
-
-  root /var/www/html;
-  index index.html;
-
-  location / {
-    try_files \$uri \$uri/ =404;
-  }
-
-  location /redirect_me {
-    return 301 'https://www.youtube.com/watch?v=axlUv9evU2k';
-  }
-
-  # Redirect error page
-  error_page 404 /404.html;
-  location = /404.html {
-    internal;
-    default_type text/html;
-    return 404 "Ceci n'est pas une page\\n";
-  }
+file {'/var/www/html/404.html':
+  ensure  => present,
+  content => 'Ceci n\'est pas une page',
 }
-EOF
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => @(EOF)
+    server {
+      listen 80 default_server;
+      listen [::]:80 default_server;
+      add_header X-Served-By \$hostname;
+
+      root /var/www/html;
+      index index.html;
+
+      location / {
+        try_files $uri $uri/ =404;
+      }
+
+      location /redirect_me {
+        return 301 'https://www.youtube.com/watch?v=axlUv9evU2k';
+      }
+
+      # Redirect error page
+      error_page 404 /404.html;
+      location = /404.html {
+        internal;
+        default_type text/html;
+        return 404 "Ceci n\'est pas une page\n";
+      }
+    }
+  EOF
 }
 
 file { '/etc/nginx/sites-enabled/default':
@@ -70,8 +54,7 @@ file { '/etc/nginx/sites-enabled/default':
 }
 
 service { 'nginx':
-  ensure    => running,
+  ensure    => 'running',
   enable    => true,
   subscribe => File['/etc/nginx/sites-available/default'],
 }
-
