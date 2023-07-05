fd24f02e1e76d8592cd69d56be909d5e1469120a
Paul John <rainson.work@gmail.com>
2023-07-05 20:19:41 +0300

    Modify manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -24,7 +24,7 @@ file { '/etc/nginx/sites-available/default':
     server {
       listen 80 default_server;
       listen [::]:80 default_server;
-      add_header X-Served-By \$hostname;
+      add_header X-Served-By $hostname;
 
       root /var/www/html;
       index index.html;
