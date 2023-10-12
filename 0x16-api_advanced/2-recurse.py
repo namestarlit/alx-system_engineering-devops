4bc74a8bfdcb02982338c10269a903abf7388bcf
Paul John <rainson.work@gmail.com>
2023-10-12 14:10:21 +0300

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -14,17 +14,25 @@ def recurse(subreddit, hot_list=[], after=""):
         List: containing titles of all hot articles for given subreddit
         None: if no results found
     """
-    res = requests.get("https://reddit.com/r/{}/hot.json?after={}"
-                       .format(subreddit, after),
-                       headers={"User-agent": "custom"})
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+
+    if next_page:
+        url += '?after={}'.format(next_page)
+    headers = {'User-Agent': 'custom'}
+
+    res = requests.get(url, headers=headers, allow_redirects=False)
 
     if res.status_code != 200:
         return None
-    if after is None:
+
+    data = res.json()['data']
+    posts = data['children']
+    for post in posts:
+        count = count + 1
+        hot_list.append(post['data']['title'])
+
+    next_page = data['after']
+    if next_page is not None:
+        return recurse(subreddit, hot_list, next_page, count)
+    else:
         return hot_list
-
-    for i in res.json().get("data").get("children"):
-        hot_list.append(i.get("data").get("title"))
-    after = res.json().get("data").get("after")
-
-    return recurse(subreddit, hot_list, after)
